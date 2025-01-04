db8803c3c427ab92066056582addc4277b0faff8
DoHyeon Kim <kdh8156@snu.ac.kr>
2025-01-04 16:14:06 +0900

    공연 검색 기능 구현  (#6)
    * merge test - generate performancehallentity.kt
    
    * feat: searchPerformance

---
+++ src/main/kotlin/com/wafflestudio/interpark/performance/persistence/PerformanceHallRepository.kt
@@ -0,0 +1,5 @@
+package com.wafflestudio.interpark.performance.persistence
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PerformanceHallRepository : JpaRepository<PerformanceHallEntity, String>
