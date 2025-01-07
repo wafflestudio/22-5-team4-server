63ad07596ef38bb4e098aa719c9b406be8b5b24b
grantzile <vladimir@snu.ac.kr>
2025-01-08 01:19:53 +0900

    feat: Mock Reply class, entity, repository
    - Not binded with performance

---
+++ src/main/kotlin/com/wafflestudio/interpark/review/controller/Reply.kt
@@ -0,0 +1,28 @@
+package com.wafflestudio.interpark.review.controller
+
+import com.wafflestudio.interpark.review.persistence.ReplyEntity
+import java.time.Instant
+
+data class Reply(
+    val id: String,
+    val reviewId: String,
+    val performance: String,
+    val author: String,
+    val content: String,
+    val createdAt: Instant,
+    val updatedAt: Instant,
+) {
+    companion object {
+        fun fromEntity(entity: ReplyEntity): Reply {
+            return Reply(
+                id = entity.id!!,
+                reviewId = entity.reviewId,
+                performance = entity.performanceId,
+                author = entity.author.id!!,
+                content = entity.content,
+                createdAt = entity.createdAt,
+                updatedAt = entity.modifiedAt
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/com/wafflestudio/interpark/review/persistence/ReplyEntity.kt
@@ -0,0 +1,31 @@
+package com.wafflestudio.interpark.review.persistence
+
+import com.wafflestudio.interpark.user.persistence.UserEntity
+import jakarta.persistence.*
+import java.time.Instant
+
+@Entity
+@Table(name = "reply")
+data class ReplyEntity(
+    @Id
+    val id: String,
+
+    @Column(name = "review_id", nullable = false)
+    val reviewId: String,
+
+    @Column(name = "performance_id", nullable = false)
+    val performanceId: String, // 공연장 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val author: UserEntity,
+
+    @Column(name = "content", nullable = false)
+    val content: String,
+
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @Column(name = "modified_at", nullable = false)
+    val modifiedAt: Instant
+)
