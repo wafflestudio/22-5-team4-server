d0dffd25b6318a6a5ac1efbac0674f4c7f7e1d35
Dohyeon Kim <kdh8156@snu.ac.kr>
2025-01-09 17:33:08 +0900

    add some Init Datas

--- src/main/kotlin/com/wafflestudio/interpark/config/DataInitializer.kt
+++ src/main/kotlin/com/wafflestudio/interpark/config/DataInitializer.kt
@@ -34,9 +34,21 @@ class DataInitializer(
                 address = "서울 서초구 남부순환로"
             )
         )
+        val SeoulArtsCenter_ConcertHall = performanceHallRepository.save(
+            PerformanceHallEntity(
+                name = "예술의전당 콘서트홀",
+                address = "서울 서초구 남부순환로"
+            )
+        )
         val LGArtCenterSeoul_SIGNATUREHAll = performanceHallRepository.save(
             PerformanceHallEntity(
                 name = "LG아트센터 서울 SIGNATURE홀",
+                address = "서울 강서구 마곡동 마곡중앙로"
+            )
+        )
+        val LGArtCenterSeoul_UPlusStage = performanceHallRepository.save(
+            PerformanceHallEntity(
+                name = "LG아트센터 서울 U+ 스테이지",
                 address = "서울 강서구 마곡동 마곡중앙로"
             )
         )
@@ -44,6 +56,24 @@ class DataInitializer(
             PerformanceHallEntity(
                 name = "올림픽공원 올림픽홀",
                 address = "서울 송파구 방이동"
+            )
+        )
+        val GoyangSportsComplex_MainStadium = performanceHallRepository.save(
+            PerformanceHallEntity(
+                name = "고양종합운동장 주경기장",
+                address = "경기도 고양시 일산서구 대화동 중앙로"
+            )
+        )
+        val SejongCenter_GrandTheater = performanceHallRepository.save(
+            PerformanceHallEntity(
+                name = "세종문화회관 대극장",
+                address = "서울 종로구 세종대로"
+            )
+        )
+        val SejongCenter_MTheater = performanceHallRepository.save(
+            PerformanceHallEntity(
+                name = "세종문화회관 M씨어터",
+                address = "서울 종로구 세종대로"
             )
         )
 
@@ -86,6 +116,7 @@ class DataInitializer(
                 seatIds = listOf("C1", "C2", "C3"),
                 reviewIds = listOf("review5")
             ),
+
             // 콘서트
             PerformanceEntity(
                 hall = BlueSquare_MasterCardHall,
@@ -100,7 +131,7 @@ class DataInitializer(
                 reviewIds = emptyList()
             ),
             PerformanceEntity(
-                hall = BlueSquare_MasterCardHall,
+                hall = OlympicPark_OlympicHall,
                 title = "2025 검정치마 단독공연",
                 detail = "SONGS TO BRING YOU HOME",
                 category = PerformanceCategory.CONCERT,
@@ -111,13 +142,97 @@ class DataInitializer(
                 seatIds = emptyList(),
                 reviewIds = emptyList()
             ),
+            PerformanceEntity(
+                hall = GoyangSportsComplex_MainStadium,
+                title = "콜드플레이 내한공연",
+                detail = "MUSIC of the SPHERES",
+                category = PerformanceCategory.CONCERT,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 4, 16), LocalDate.of(2025, 4, 25)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
+
             // 클래식
+            PerformanceEntity(
+                hall = SeoulArtsCenter_ConcertHall,
+                title = "브루스 리우 피아노 리사이틀",
+                detail = "TCHAIKOVSKY | MENDELSSOHN | SCRIABIN | PROKOFIEV",
+                category = PerformanceCategory.CLASSIC,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 5, 11)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
+            PerformanceEntity(
+                hall = SeoulArtsCenter_ConcertHall,
+                title = "크리스티안 테츨라프 바이올린 리사이틀",
+                detail = "SUK | BRAHMS | SZYMANOWSKI | FRANCK",
+                category = PerformanceCategory.CLASSIC,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 5, 1)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
+            PerformanceEntity(
+                hall = SejongCenter_GrandTheater,
+                title = "발레의 별빛, 글로벌 발레스타 초청 갈라공연",
+                detail = "전 세계가 먼저 찾는 한국 스타 무용수들의 향연!",
+                category = PerformanceCategory.CLASSIC,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 1, 11), LocalDate.of(2025, 1, 12)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
+
             // 연극
+            PerformanceEntity(
+                hall = LGArtCenterSeoul_UPlusStage,
+                title = "연극 애나엑스",
+                detail = "ANNA X",
+                category = PerformanceCategory.PLAY,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 1, 28), LocalDate.of(2025, 3, 16)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
+            PerformanceEntity(
+                hall = LGArtCenterSeoul_UPlusStage,
+                title = "연극 타인의 삶",
+                detail = "영화 타인의 삶 원작",
+                category = PerformanceCategory.PLAY,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 1, 28), LocalDate.of(2025, 3, 16)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
+            PerformanceEntity(
+                hall = SejongCenter_MTheater,
+                title = "세일즈맨의 죽음",
+                detail = "현 희곡의 거장 '아서 밀러'의 대표작 연극<세일즈맨의 죽음>이 돌아왔다!",
+                category = PerformanceCategory.PLAY,
+                sales = 0,
+                dates = listOf(LocalDate.of(2025, 1, 7), LocalDate.of(2025, 3, 3)),
+                posterUrl = "http://example.com/poster/mom.jpg",
+                backdropUrl = "http://example.com/backdrop/mom.jpg",
+                seatIds = emptyList(),
+                reviewIds = emptyList()
+            ),
         )
 
         // 3) 한번에 저장
         performanceRepository.saveAll(performanceList)
-
-        // 이제 DB에 3개의 PerformanceEntity가 insert됨
     }
 }
