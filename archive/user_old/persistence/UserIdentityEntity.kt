3c72299b3325ca4fb89f571d9de86a515303c377
Dohyeon Kim <kdh8156@snu.ac.kr>
2025-01-18 15:31:58 +0900

    backup <user> package to <archive/user_old>

---
+++ archive/user_old/persistence/UserIdentityEntity.kt
@@ -0,0 +1,31 @@
+package com.wafflestudio.interpark.user.persistence
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+
+@Entity
+class UserIdentityEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String? = null,
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    var user: UserEntity,
+    @Column(name = "role", nullable = false)
+    var role: UserRole = UserRole.USER,
+    @Column(name = "hashed_password", nullable = false)
+    val hashedPassword: String,
+    @Column(name = "provider", nullable = false)
+    val provider: String,
+    @Column(name = "social_id", nullable = true)
+    val socialId: String? = null,
+)
+
+enum class UserRole {
+    USER, ADMIN
+}
---
+++ archive/user_old/service/UserService.kt
@@ -0,0 +1,95 @@
+package com.wafflestudio.interpark.user.service
+
+import com.wafflestudio.interpark.user.*
+import com.wafflestudio.interpark.user.controller.User
+import com.wafflestudio.interpark.user.persistence.UserEntity
+import com.wafflestudio.interpark.user.persistence.UserIdentityEntity
+import com.wafflestudio.interpark.user.persistence.UserIdentityRepository
+import com.wafflestudio.interpark.user.persistence.UserRepository
+import com.wafflestudio.interpark.user.persistence.UserRole
+import org.mindrot.jbcrypt.BCrypt
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+    private val userIdentityRepository: UserIdentityRepository,
+    private val userAccessTokenUtil: UserAccessTokenUtil,
+) {
+    @Transactional
+    fun signUp(
+        username: String,
+        password: String,
+        nickname: String,
+        phoneNumber: String,
+        email: String,
+        role: UserRole = UserRole.USER,
+    ): User {
+        if (username.length < 6 || username.length > 20) {
+            throw SignUpBadUsernameException()
+        }
+        if (password.length < 8 || password.length > 12) {
+            throw SignUpBadPasswordException()
+        }
+        if (userRepository.existsByUsername(username)) {
+            throw SignUpUsernameConflictException()
+        }
+        val encryptedPassword = BCrypt.hashpw(password, BCrypt.gensalt())
+        val user =
+            userRepository.save(
+                UserEntity(
+                    username = username,
+                    nickname = nickname,
+                    phoneNumber = phoneNumber,
+                    email = email,
+                ),
+            )
+        userIdentityRepository.save(
+            UserIdentityEntity(
+                user = user,
+                role = role,
+                hashedPassword = encryptedPassword,
+                provider = "self",
+            ),
+        )
+        return User.fromEntity(user)
+    }
+
+    @Transactional
+    fun signIn(
+        username: String,
+        password: String,
+    ): Pair<String, String> {
+        val targetUser = userRepository.findByUsername(username) ?: throw SignInUserNotFoundException()
+        val targetIdentity = userIdentityRepository.findByUser(targetUser) ?: throw SignInUserNotFoundException()
+        if (!BCrypt.checkpw(password, targetIdentity.hashedPassword)) {
+            throw SignInInvalidPasswordException()
+        }
+        val accessToken = userAccessTokenUtil.generateAccessToken(targetUser.id!!)
+        val refreshToken = userAccessTokenUtil.generateRefreshToken(targetIdentity.id!!)
+        return Pair(accessToken, refreshToken)
+    }
+
+    @Transactional
+    fun signOut(refreshToken: String) {
+        userAccessTokenUtil.removeRefreshToken(refreshToken)
+    }
+
+    @Transactional
+    fun authenticate(accessToken: String): User {
+        val userId = userAccessTokenUtil.validateAccessToken(accessToken) ?: throw AuthenticateException()
+        val user = userRepository.findByIdOrNull(userId) ?: throw AuthenticateException()
+        return User.fromEntity(user)
+    }
+
+    @Transactional
+    fun refreshAccessToken(refreshToken: String): Pair<String, String> {
+        return userAccessTokenUtil.refreshAccessToken(refreshToken) ?: throw AuthenticateException()
+    }
+
+    fun getUserIdentityByUserId(userId: String): UserIdentityEntity? {
+        return userIdentityRepository.findByUserId(userId)
+    }
+}
