bf6b0ca7ee5ed134dc408de3f3c6e73b524cf408
ChungPlusPlus <jjy051221@gmail.com>
2025-01-20 01:05:07 +0900

    feat: Reinforce Simultaneous test code
    동시에 여러 사람의 접근이 있어도 통과하는지 테스트 코드 추가
    동시에 여러 사람이 서로 다른 좌석에 접근할 때 테스트 코드 추가

--- src/test/kotlin/com/wafflestudio/interpark/SimultaneousTest.kt
+++ src/test/kotlin/com/wafflestudio/interpark/SimultaneousTest.kt
@@ -97,8 +97,8 @@ constructor(
             }
 
         val results = mutableListOf<Int>()
-        var successCnt = AtomicInteger(0)
-        var conflictCnt = AtomicInteger(0)
+        val successCnt = AtomicInteger(0)
+        val conflictCnt = AtomicInteger(0)
         val tasks = (1..10).map {
             threadPool.submit {
                 val responseStatus = mvc.perform(
@@ -121,5 +121,199 @@ constructor(
         tasks.forEach { it.get() }
         assert(successCnt.get() == 1) {"expected 1 success but ${successCnt.get()}"}
         assert(conflictCnt.get() == 9) {"expected 9 conflict but ${conflictCnt.get()}"}
+    }
+
+    @Test
+    fun `한 예매에 동시에 여러명이 접속해도 하나만 통과한다`() {
+        val threadPool = Executors.newFixedThreadPool(10)
+        val username = (0..9).map { UUID.randomUUID().toString().take(16) }
+        val password = "password123"
+
+        // 1️⃣ 회원가입
+        (0..9).map {
+            mvc.perform(
+                post("/api/v1/local/signup")
+                    .content(
+                        mapper.writeValueAsString(
+                            mapOf(
+                                "username" to username[it],
+                                "password" to password,
+                                "nickname" to "reviewer",
+                                "phoneNumber" to "010-0000-0000",
+                                "email" to "reviewer@example.com",
+                            ),
+                        ),
+                    )
+                    .contentType(MediaType.APPLICATION_JSON),
+            ).andExpect(status().`is`(200))
+        }
+
+        // 2️⃣ 로그인 → 토큰 획득
+        val accessToken =
+            (0..9).map {
+                mvc.perform(
+                    post("/api/v1/local/signin")
+                        .content(
+                            mapper.writeValueAsString(
+                                mapOf(
+                                    "username" to username[it],
+                                    "password" to password,
+                                ),
+                            ),
+                        )
+                        .contentType(MediaType.APPLICATION_JSON),
+                ).andExpect(status().`is`(200))
+                    .andReturn()
+                    .response
+                    .getContentAsString(Charsets.UTF_8)
+                    .let { mapper.readTree(it).get("accessToken").asText() }
+            }
+
+        val performanceEventId =
+            mvc.perform(
+                get("/api/v1/performance-event")
+                    .header("Authorization", "Bearer ${accessToken[0]}"),
+            ).andExpect(status().`is`(200))
+                .andReturn()
+                .response
+                .getContentAsString(Charsets.UTF_8)
+                .let {
+                    val performanceEvents = mapper.readTree(it)
+                    performanceEvents[0].get("id").asText()
+                }
+
+        val reservationId = mvc.perform(
+            get("/api/v1/seat/$performanceEventId/available")
+        ).andExpect(status().`is`(200))
+            .andReturn()
+            .response
+            .getContentAsString(Charsets.UTF_8)
+            .let {
+                val availableSeats = mapper.readTree(it).get("availableSeats")
+                availableSeats[0].get("reservationId").asText()
+            }
+
+        val results = mutableListOf<Int>()
+        val successCnt = AtomicInteger(0)
+        val conflictCnt = AtomicInteger(0)
+        val tasks = (0..9).map {
+            threadPool.submit {
+                val responseStatus = mvc.perform(
+                    post("/api/v1/reservation/reserve")
+                        .content(
+                            mapper.writeValueAsString(
+                                mapOf(
+                                    "reservationId" to reservationId,
+                                ),
+                            ),
+                        )
+                        .header("Authorization", "Bearer ${accessToken[it]}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andReturn().response.status
+                results.add(responseStatus)
+                if (responseStatus == 200) { successCnt.incrementAndGet() }
+                if (responseStatus == 409) { conflictCnt.incrementAndGet() }
+            }
+        }
+        tasks.forEach { it.get() }
+        assert(successCnt.get() == 1) {"expected 1 success but ${successCnt.get()}"}
+        assert(conflictCnt.get() == 9) {"expected 9 conflict but ${conflictCnt.get()}"}
+    }
+
+    @Test
+    fun `동시에 다른 좌석에 접근하면 모두 잘 처리된다`() {
+        val threadPool = Executors.newFixedThreadPool(10)
+        val username = (0..9).map { UUID.randomUUID().toString().take(15) }
+        val password = "password123"
+
+        // 1️⃣ 회원가입
+        (0..9).map {
+            mvc.perform(
+                post("/api/v1/local/signup")
+                    .content(
+                        mapper.writeValueAsString(
+                            mapOf(
+                                "username" to username[it],
+                                "password" to password,
+                                "nickname" to "reviewer",
+                                "phoneNumber" to "010-0000-0000",
+                                "email" to "reviewer@example.com",
+                            ),
+                        ),
+                    )
+                    .contentType(MediaType.APPLICATION_JSON),
+            ).andExpect(status().`is`(200))
+        }
+
+        // 2️⃣ 로그인 → 토큰 획득
+        val accessToken =
+            (0..9).map {
+                mvc.perform(
+                    post("/api/v1/local/signin")
+                        .content(
+                            mapper.writeValueAsString(
+                                mapOf(
+                                    "username" to username[it],
+                                    "password" to password,
+                                ),
+                            ),
+                        )
+                        .contentType(MediaType.APPLICATION_JSON),
+                ).andExpect(status().`is`(200))
+                    .andReturn()
+                    .response
+                    .getContentAsString(Charsets.UTF_8)
+                    .let { mapper.readTree(it).get("accessToken").asText() }
+            }
+
+        val performanceEventId =
+            mvc.perform(
+                get("/api/v1/performance-event")
+                    .header("Authorization", "Bearer ${accessToken[0]}"),
+            ).andExpect(status().`is`(200))
+                .andReturn()
+                .response
+                .getContentAsString(Charsets.UTF_8)
+                .let {
+                    val performanceEvents = mapper.readTree(it)
+                    performanceEvents[0].get("id").asText()
+                }
+
+        val reservationId = mvc.perform(
+            get("/api/v1/seat/$performanceEventId/available")
+        ).andExpect(status().`is`(200))
+            .andReturn()
+            .response
+            .getContentAsString(Charsets.UTF_8)
+            .let {
+                val availableSeats = mapper.readTree(it).get("availableSeats")
+                (1..10).map { availableSeats[it].get("reservationId").asText() }
+            }
+
+        val results = mutableListOf<Int>()
+        val successCnt = AtomicInteger(0)
+        val conflictCnt = AtomicInteger(0)
+        val tasks = (0..9).map {
+            threadPool.submit {
+                val responseStatus = mvc.perform(
+                    post("/api/v1/reservation/reserve")
+                        .content(
+                            mapper.writeValueAsString(
+                                mapOf(
+                                    "reservationId" to reservationId[it],
+                                ),
+                            ),
+                        )
+                        .header("Authorization", "Bearer ${accessToken[it]}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andReturn().response.status
+                results.add(responseStatus)
+                if (responseStatus == 200) { successCnt.incrementAndGet() }
+                if (responseStatus == 409) { conflictCnt.incrementAndGet() }
+            }
+        }
+        tasks.forEach { it.get() }
+        assert(successCnt.get() == 10) {"expected 10 success but ${successCnt.get()}"}
+        assert(conflictCnt.get() == 0) {"expected 0 conflict but ${conflictCnt.get()}"}
     }
 }
