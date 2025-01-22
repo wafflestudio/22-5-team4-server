181f2e41ba823bb68ba0d4b2245650ed2c35de2e
Dohyeon Kim <kdh8156@snu.ac.kr>
2025-01-22 21:22:46 +0900

    throw AuthenticateException when accessToken is not valid

--- src/main/kotlin/com/wafflestudio/interpark/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/wafflestudio/interpark/security/JwtAuthenticationFilter.kt
@@ -1,11 +1,10 @@
 package com.wafflestudio.interpark.security
 
+import com.wafflestudio.interpark.user.AuthenticateException
 import com.wafflestudio.interpark.user.UserAccessTokenUtil
-import com.wafflestudio.interpark.user.controller.UserDetailsImpl
 import com.wafflestudio.interpark.user.service.UserDetailsServiceImpl
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.web.filter.OncePerRequestFilter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
@@ -30,21 +29,19 @@ class JwtAuthenticationFilter(
             val accessToken = header.split(" ")[1]
 
             // 2) UserAccessTokenUtil로 토큰 유효성 검사
-            //    유효하면 userId가 반환되고, 유효하지 않으면 null
-            val subject = userAccessTokenUtil.validateAccessToken(accessToken)
+            //    유효하면 userId가 반환되고, 유효하지 않으면 AuthenticateException 던짐
+            val subject = userAccessTokenUtil.validateAccessToken(accessToken) ?: throw AuthenticateException()
 
-            if (subject != null) {
-                // 3) subject(userId)를 기반으로 DB에서 유저/권한 정보 조회 (UserDetails)
-                val userDetails = userDetailsService.loadUserByUserId(subject)
+            // 3) subject(userId)를 기반으로 DB에서 유저/권한 정보 조회 (UserDetails)
+            val userDetails = userDetailsService.loadUserByUserId(subject)
 
-                // 4) Spring Security에 Authentication 등록
-                val authentication = UsernamePasswordAuthenticationToken(
-                    userDetails,
-                    null,
-                    userDetails.authorities
-                )
-                SecurityContextHolder.getContext().authentication = authentication
-            }
+            // 4) Spring Security에 Authentication 등록
+            val authentication = UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.authorities
+            )
+            SecurityContextHolder.getContext().authentication = authentication
         }
 
         // 5) 체인 계속 진행
